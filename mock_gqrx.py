78a12522bf1329c8089722c919be28f79c5b629f
Shane Wirkes <swirkes@gmail.com>
2023-08-18 14:02:57 +0300

    Refactored python code.

---
+++ mock_gqrx.py
@@ -0,0 +1,10 @@
+import socket
+
+class MockGqrxTCPInterface:
+    """Mock interface to simulate Gqrx interactions."""
+    # ... (same as in the original code)
+
+class MockGqrxTCPServer:
+    """Mock TCP server to simulate Gqrx server."""
+    # ... (same as in the original code)
+
---
+++ test_gqrx.py
@@ -0,0 +1,9 @@
+from gqrx_controller import GqrxController
+import ipywidgets as widgets
+
+def test_gqrx_connection():
+    """Test the connection to the Gqrx server."""
+    # ... (same as in the original code)
+
+# ... (rest of the test and demonstration code)
+
